9007d21dc689914458de6591466d4c71d1ef4861
Vignesh Thiyagarajan <mailvigneshthiyagarajan@gmail.com>
2019-01-10 22:58:35 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseComponent } from './expense/expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseComponent
   ],
   imports: [
     BrowserModule,
